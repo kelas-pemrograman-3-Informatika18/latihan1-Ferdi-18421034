8ec7c1f8e40d326fb31cee1602b81142343d2971
Muhammad Ferdi Nurichwan <m.ferdi.18421034@student.ubl.ac.id>
2020-03-26 14:21:59 +0700

    first commit

---
+++ latihanquasar/src/pages/loginpage.vue
@@ -0,0 +1,71 @@
+<template>
+<q-page class="bg-blue-1">
+<div class ="row">
+<q-card class="fixed-center col-md-4 col-xs-12 bg-white" flat>
+<q-card-section>
+<div class="text-h6 q-pb-md">
+Halaman Login
+</div>
+<q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="username"
+        label="Username Anda"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Tolong ketik username']"
+      />
+
+      <q-input
+        filled
+        type="password"
+        v-model="password"
+        label="password anda"
+        lazy-rules
+        :rules="[
+          val => val !== null && val > 0 || 'Password tidak boleh kosong'
+        ]"
+      />
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+    </q-card-section>
+    </q-card>
+    </div>
+</q-page>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      username: null,
+      password: null
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.username === 'ferdi' && this.password === '18421034') {
+        this.$q.notify({
+          type: 'positive',
+          message: 'Selamat Anda Berhasil Login'
+        })
+        this.$router.push('/home')
+      } else {
+        this.$q.notify({
+          type: 'negative',
+          message: 'Gagal Login, username/password salah'
+        })
+      }
+    },
+    onReset () {
+      this.username = null
+      this.password = null
+    }
+  }
+}
+</script>
---
+++ latihanquasar/src/pages/register.vue
@@ -0,0 +1,81 @@
+<template>
+<q-page>
+Halaman Register
+<q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="username"
+        label="Username Anda"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Tolong ketik username']"
+      />
+
+      <q-input
+        filled
+        type="password"
+        v-model="password"
+        label="Buat Password"
+        lazy-rules
+        :rules="[
+          val => val !== null && val > 0 || 'Password tidak boleh kosong'
+        ]"
+      />
+
+      <q-input
+        filled
+        label="Masukan Alamat Anda"
+        lazy-rules
+        :rules="[
+          val => val !== null && val > 0 || 'Alamat tidak boleh kosong'
+        ]"
+      />
+
+       <div class="q-pa-md q-gutter-sm">
+    <div class="q-gutter-sm">
+      <q-radio dense v-model="shape" val="Sudah Menikah" label="Sudah Menikah" />
+      <q-radio dense v-model="shape" val="Belum Menikah" label="Belum Menikah" />
+    </div>
+
+    <div class="q-px-sm q-pt-sm">
+      Anda Ternyata: <strong>{{ shape }}</strong>
+    </div>
+  </div>
+
+  <q-input
+        filled
+        label="Masukan Nomor Telepon Anda"
+        lazy-rules
+        :rules="[
+          val => val !== null && val > 0 || 'Nomor Tidak boleh kosong'
+        ]"
+      />
+
+      <div>
+        <q-btn label="Daftar" type="submit" color="purple"/>
+        <q-btn label="Reset" type="reset" color="red" />
+      </div>
+    </q-form>
+</q-page>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      username: null,
+      password: null
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('ini fungsi submit')
+    },
+    onReset () {
+      console.log('ini fungsi reset')
+    }
+  }
+}
+</script>
